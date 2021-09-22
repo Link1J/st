03826734f48dc1da54d8e822a22333609f83603e
Jared Irwin <jrairwin@sympatico.ca>
2021-09-22 13:08:13 -0400

    add undercurl

--- st.h
+++ st.h
@@ -22,21 +22,22 @@
 #define IS_TRUECOL(x)		(1 << 24 & (x))
 
 enum glyph_attribute {
-	ATTR_NULL       = 0,
-	ATTR_BOLD       = 1 << 0,
-	ATTR_FAINT      = 1 << 1,
-	ATTR_ITALIC     = 1 << 2,
-	ATTR_UNDERLINE  = 1 << 3,
-	ATTR_BLINK      = 1 << 4,
-	ATTR_REVERSE    = 1 << 5,
-	ATTR_INVISIBLE  = 1 << 6,
-	ATTR_STRUCK     = 1 << 7,
-	ATTR_WRAP       = 1 << 8,
-	ATTR_WIDE       = 1 << 9,
-	ATTR_WDUMMY     = 1 << 10,
-	ATTR_LIGA       = 1 << 11,
-	ATTR_BOXDRAW    = 1 << 12,
-	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
+	ATTR_NULL           = 0,
+	ATTR_BOLD           = 1 << 0,
+	ATTR_FAINT          = 1 << 1,
+	ATTR_ITALIC         = 1 << 2,
+	ATTR_UNDERLINE      = 1 << 3,
+	ATTR_BLINK          = 1 << 4,
+	ATTR_REVERSE        = 1 << 5,
+	ATTR_INVISIBLE      = 1 << 6,
+	ATTR_STRUCK         = 1 << 7,
+	ATTR_WRAP           = 1 << 8,
+	ATTR_WIDE           = 1 << 9,
+	ATTR_WDUMMY         = 1 << 10,
+	ATTR_LIGA           = 1 << 11,
+	ATTR_BOXDRAW        = 1 << 12,
+	ATTR_DIRTYUNDERLINE = 1 << 15,
+	ATTR_BOLD_FAINT     = ATTR_BOLD | ATTR_FAINT,
 };
 
 enum selection_mode {
@@ -68,6 +69,8 @@ typedef struct {
 	ushort mode;      /* attribute flags */
 	uint32_t fg;      /* foreground  */
 	uint32_t bg;      /* background  */
+	int ustyle;	  /* underline style */
+	int ucolor[3];    /* underline color */
 } Glyph;
 
 typedef Glyph *Line;
